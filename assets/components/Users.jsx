fe927eb28506b1e5dabd3bc21ec325d4b8234023
anthonyDeplanque <anthonyDeplanque@gmail.com>
2021-06-29 16:36:56 +0200

    post de mes couilles !

--- assets/components/Users.jsx
+++ assets/components/Users.jsx
@@ -1,3 +1,4 @@
+import axios from "axios";
 import React, { useContext, useEffect, useState } from "react";
 import UserContext from "../context/UserContext";
 
@@ -14,29 +15,33 @@ export default (Users) => {
     setter(null);
   };
 
+  const handleSubmit = () => {
+    axios.get(`/api/user?language=${userLanguage}&hourlyRate=${userHourlyRate}&category=${userCategories}`).then((r)=> r.config.data);
+  };
   return (
     <>
       <h4>selection</h4>
       <ul>
         {userLanguage && (
           <li>
-            Language selected : {userLanguage}{' '}
+            Language selected : {userLanguage}{" "}
             <button onClick={() => handleClick(setUserLanguage)}>X</button>
           </li>
         )}
         {userHourlyRate && (
           <li>
-            Hourly Rate selected : {userHourlyRate}{' '}
+            Hourly Rate selected : {userHourlyRate}{" "}
             <button onClick={() => handleClick(setUserHourlyRate)}>X</button>
           </li>
         )}
         {userCategories && (
           <li>
-            Category selected : {userCategories}{' '}
+            Category selected : {userCategories}{" "}
             <button onClick={() => handleClick(setUserCategories)}>X</button>
           </li>
         )}
       </ul>
+      <button onClick = {()=> handleSubmit()}>submit</button>
     </>
   );
 };
