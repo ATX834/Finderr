a4cfba6bdcbf938bcc729bbf6a80820d359aef92
anthonyDeplanque <anthonyDeplanque@gmail.com>
2021-06-29 15:41:56 +0200

    suppression du contexte de ma bite'

--- assets/components/Users.jsx
+++ assets/components/Users.jsx
@@ -2,14 +2,41 @@ import React, { useContext, useEffect, useState } from "react";
 import UserContext from "../context/UserContext";
 
 export default (Users) => {
-const {userLanguage, userHourlyRate, userCategories} = useContext(UserContext)
+  const {
+    userLanguage,
+    userHourlyRate,
+    userCategories,
+    setUserLanguage,
+    setUserHourlyRate,
+    setUserCategories,
+  } = useContext(UserContext);
+  const handleClick = (setter) => {
+    setter(null);
+  };
 
   return (
     <>
-      <h1>Users</h1>
-      {userLanguage && <p>Language selected : {userLanguage}</p>}
-      {userHourlyRate && <p>Hourly Rate selected : {userHourlyRate}</p>}
-      {userCategories && <p>Category selected : {userCategories}</p>}
+      <h4>selection</h4>
+      <ul>
+        {userLanguage && (
+          <li>
+            Language selected : {userLanguage}{' '}
+            <button onClick={() => handleClick(setUserLanguage)}>X</button>
+          </li>
+        )}
+        {userHourlyRate && (
+          <li>
+            Hourly Rate selected : {userHourlyRate}{' '}
+            <button onClick={() => handleClick(setUserHourlyRate)}>X</button>
+          </li>
+        )}
+        {userCategories && (
+          <li>
+            Category selected : {userCategories}{' '}
+            <button onClick={() => handleClick(setUserCategories)}>X</button>
+          </li>
+        )}
+      </ul>
     </>
   );
 };
