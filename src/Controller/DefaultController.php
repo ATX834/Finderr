7aa65540f94b2f8c9f8adbadbd4158fee246626d
PierreCapel <pecapel@gmail.com>
2021-06-29 16:51:55 +0200

    wip

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,12 +2,27 @@
 
 namespace App\Controller;
 
+use App\Repository\UserRepository;
+use App\Services\PicturesGenerator;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DefaultController extends AbstractController
 {
+
+    /**
+     * @Route("/search-results", name="search_result",)
+     */
+    public function searchResults(UserRepository $userRepository, PicturesGenerator $picturesGenerator ): Response
+    {
+        $users = $userRepository->findAll();
+        return $this->render('default/searchResults.html.twig', [
+            'users' => $users,
+            'pictures' => $picturesGenerator->generateRandomImg()
+        ]);
+    }
+
     /**
      * @Route("/{reactRouting}", name="default", defaults={"reactRouting": null})
      */
