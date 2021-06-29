ab73f1075d62932519d648b0a29347b92032cdc8
anthonyDeplanque <anthonyDeplanque@gmail.com>
2021-06-29 15:31:58 +0200

    userContext de mes couilles'

--- assets/components/Users.jsx
+++ assets/components/Users.jsx
@@ -2,37 +2,14 @@ import React, { useContext, useEffect, useState } from "react";
 import UserContext from "../context/UserContext";
 
 export default (Users) => {
-  const [users, setUsers] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const { userId, setUserId } = useContext(UserContext);
-
-  const fetchUsers = async () => {
-    const response = await fetch("/api/user");
-    const jsonData = await response.json();
-    setUsers(jsonData);
-    setLoading(false);
-  };
-  useEffect(() => {
-    fetchUsers();
-  }, []);
-
-  if (loading) {
-    return <p>Chargement des utilisateurs ...</p>;
-  }
+const {userLanguage, userHourlyRate, userCategories} = useContext(UserContext)
 
   return (
     <>
       <h1>Users</h1>
-
-      <p>hello ! </p>
-
-      <ul>
-        {users.map((user) => (
-          <li>
-            {user.firstname} {user.lastname}, {user.age}
-          </li>
-        ))}
-      </ul>
+      {userLanguage && <p>Language selected : {userLanguage}</p>}
+      {userHourlyRate && <p>Hourly Rate selected : {userHourlyRate}</p>}
+      {userCategories && <p>Category selected : {userCategories}</p>}
     </>
   );
 };
